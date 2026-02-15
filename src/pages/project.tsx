27262a461aa2db08521f262ca1150100dab6178f
abrtwcom <amitbartwal008@gmail.com>
2026-02-15 20:18:19 +0530

    add project page

---
+++ src/pages/project.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+
+const page: NextPage = () => {  
+    return (
+<>
+<div className="flex flex-col items-center justify-center h-screen">
+
+
+</div>
+</> 
+    );
+};
+
+export default page;
+
